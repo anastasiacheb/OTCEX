7f2006074f91d396bc4e4129aef79eccd1d984c9
Anastasia <161876455+anastasiacheb@users.noreply.github.com>
2025-10-14 08:07:25 +0300

    footer

--- src/js/main.js
+++ src/js/main.js
@@ -30,6 +30,12 @@ let selectText = document.querySelector('.js-select-text');
 selectButton.addEventListener('click', () => {
   selectContainer.classList.toggle('hidden');
   selectOverlay.classList.toggle('hidden');
+
+  nav.classList.remove('left-0');
+  nav.classList.add('-left-full');
+  nav2.classList.remove('bg-light');
+  navClose.classList.add('hidden');
+  navOpen.classList.remove('hidden');
 });
 
 selectClose.addEventListener('click', () => {
@@ -42,5 +48,24 @@ for (let i = 0; i < selectVariants.length; i++) {
     selectContainer.classList.add('hidden');
     selectOverlay.classList.add('hidden');
     selectText.textContent = selectVariants[i].textContent;
+  });
+}
+
+let infoBnt = document.querySelectorAll('.js-info-btn');
+let infoOverlay = document.querySelectorAll('.js-info-overlay');
+let infoCnt = document.querySelectorAll('.js-info-cnt');
+let infoClose = document.querySelectorAll('.js-info-close');
+
+for (let i = 0; i < infoBnt.length; i++) {
+  infoBnt[i].addEventListener('click', () => {
+    infoOverlay[i].classList.remove('hidden');
+    infoCnt[i].classList.remove('hidden');
+  });
+}
+
+for (let i = 0; i < infoClose.length; i++) {
+  infoClose[i].addEventListener('click', () => {
+    infoOverlay[i].classList.add('hidden');
+    infoCnt[i].classList.add('hidden');
   });
 }
